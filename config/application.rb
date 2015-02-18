2b5583c419de96dcdd7e0be69bdb5db4b8bb17b6
Trey Jackson <jacksrf@gmail.com>
2015-02-17 21:56:35 -0500

    updated navbar with dropdown

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ require 'semantics3'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module OneList20
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -25,5 +27,6 @@ module OneList20
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
